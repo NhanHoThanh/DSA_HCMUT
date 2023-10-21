3db6ee78009d7c8f3b26cb88a506ce1392c30ec0
thanhnhanho <hothanhnhanhot@gmail.com>
2023-10-21 23:04:49 +0700

    Stuck on the iterator

--- Lab2/doublelinkedlist.cpp
+++ Lab2/doublelinkedlist.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 template <class T>
 class DLinkedList
 {
@@ -86,3 +89,164 @@ public:
         Iterator operator++(int);
     };
 };
+
+/*
+ * TODO: Implement class Iterator's method
+ * Note: method remove is different from SLinkedList, which is the advantage of DLinkedList
+ */
+template <class T>
+DLinkedList<T>::Iterator::Iterator(DLinkedList<T> *pList, bool begin)
+{
+    this->pList = pList;
+    if (begin)
+    {
+        if (pList != NULL)
+        {
+            this->current = pList->head;
+            index = 0;
+        }
+        else
+        {
+            this->current = nullptr;
+            index = -1;
+        }
+    }
+    else
+    {
+        this->current = nullptr;
+        if (pList != NULL)
+        {
+            index = pList->size();
+        }
+        else
+        {
+            index = 0;
+        }
+    }
+}
+
+template <class T>
+typename DLinkedList<T>::Iterator &DLinkedList<T>::Iterator::operator=(const DLinkedList<T>::Iterator &iterator)
+{
+    this->current = iterator.current;
+    this->index = iterator.index;
+    this->pList = iterator.pList;
+    return *this;
+}
+
+template <class T>
+void DLinkedList<T>::Iterator::set(const T &e)
+{
+    if (current == NULL)
+    {
+        throw std::out_of_range("Segmentation fault!");
+    }
+    else
+    {
+        current->data = e;
+    }
+}
+
+template <class T>
+T &DLinkedList<T>::Iterator::operator*()
+{
+    if (current == NULL)
+    {
+
+        throw std::out_of_range("Segmentation fault!");
+    }
+    else
+    {
+        return current->data;
+    }
+}
+
+template <class T>
+void DLinkedList<T>::Iterator::remove()
+{
+    /*
+     * TODO: delete Node in pList which Node* current point to.
+     *       After that, Node* current point to the node before the node just deleted.
+     *       If we remove first node of pList, Node* current point to nullptr.
+     *       Then we use operator ++, Node* current will point to the head of pList.
+     */
+    if (current == nullptr)
+    {
+        return;
+    }
+
+    Node *nodeToRemove = current;
+    current = current->previous;
+
+    if (current != nullptr)
+    {
+        current->next = nodeToRemove->next;
+    }
+    else
+    {
+        pList->head = nodeToRemove->next;
+    }
+
+    if (nodeToRemove->next != nullptr)
+    {
+        nodeToRemove->next->previous = current;
+    }
+    else
+    {
+        pList->tail = current;
+    }
+
+    delete nodeToRemove;
+    pList->count--;
+}
+
+template <class T>
+bool DLinkedList<T>::Iterator::operator!=(const DLinkedList::Iterator &iterator)
+{
+    if (this->pList == iterator.pList && this->current == iterator.current && this->index == iterator.index)
+        return false;
+    return true;
+}
+
+template <class T>
+typename DLinkedList<T>::Iterator &DLinkedList<T>::Iterator::operator++()
+{
+    current = current->next;
+    if (current == nullptr)
+    {
+        current = pList->head;
+    }
+    index++;
+    return *this;
+}
+
+template <class T>
+typename DLinkedList<T>::Iterator DLinkedList<T>::Iterator::operator++(int)
+{
+    Iterator temp = *this;
+    current = current->next;
+    if (current == nullptr)
+    {
+        current = pList->head;
+    }
+    if (current == nullptr)
+    {
+        throw std::out_of_range("Segmentation fault!");
+    }
+    return temp;
+}
+
+int main()
+{
+    DLinkedList<int> list;
+    int size = 10;
+    for (int idx = 0; idx < size; idx++)
+    {
+        list.add(idx);
+    }
+    DLinkedList<int>::Iterator it = list.begin();
+    for (; it != list.end(); it++)
+    {
+        cout << *it << " |";
+    }
+}

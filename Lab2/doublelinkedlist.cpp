48e82e9fc9c86369b8a496832ad98f54d2cf2c4c
thanhnhanho <hothanhnhanhot@gmail.com>
2023-10-21 19:36:35 +0700

    First Commit

---
+++ Lab2/doublelinkedlist.cpp
@@ -0,0 +1,88 @@
+template <class T>
+class DLinkedList
+{
+public:
+    class Iterator; // forward declaration
+    class Node;     // forward declaration
+protected:
+    Node *head;
+    Node *tail;
+    int count;
+
+public:
+    DLinkedList() : head(NULL), tail(NULL), count(0){};
+    ~DLinkedList();
+    void add(const T &e);
+    void add(int index, const T &e);
+    T removeAt(int index);
+    bool removeItem(const T &item);
+    bool empty();
+    int size();
+    void clear();
+    T get(int index);
+    void set(int index, const T &e);
+    int indexOf(const T &item);
+    bool contains(const T &item);
+    string toString();
+    Iterator begin()
+    {
+        return Iterator(this, true);
+    }
+    Iterator end()
+    {
+        return Iterator(this, false);
+    }
+
+public:
+    class Node
+    {
+    private:
+        T data;
+        Node *next;
+        Node *previous;
+        friend class DLinkedList<T>;
+
+        Iterator begin()
+        {
+            return Iterator(this, true);
+        }
+        Iterator end()
+        {
+            return Iterator(this, false);
+        }
+
+    public:
+        Node()
+        {
+            this->previous = NULL;
+            this->next = NULL;
+        }
+
+        Node(const T &data)
+        {
+            this->data = data;
+            this->previous = NULL;
+            this->next = NULL;
+        }
+    };
+    class Iterator
+    {
+    private:
+        DLinkedList<T> *pList;
+        Node *current;
+        int index; // is the index of current in pList
+    public:
+        Iterator(DLinkedList<T> *pList, bool begin);
+        Iterator &operator=(const Iterator &iterator);
+        void set(const T &e);
+        T &operator*();
+        bool operator!=(const Iterator &iterator);
+        void remove();
+
+        // Prefix ++ overload
+        Iterator &operator++();
+
+        // Postfix ++ overload
+        Iterator operator++(int);
+    };
+};

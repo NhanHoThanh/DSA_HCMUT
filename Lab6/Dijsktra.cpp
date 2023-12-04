37cb3227142a6a8872b15d1b4a49716c4229430b
thanhnhanho <hothanhnhanhot@gmail.com>
2023-12-04 19:56:21 +0700

    finish Lab6

--- Lab6/Dijsktra.cpp
+++ Lab6/Dijsktra.cpp
@@ -1,5 +1,27 @@
-// Some helping functions
+#include <bits/stdc++.h>
+using namespace std;
 
 int Dijkstra(int** graph, int src, int dst) {
-    // TODO: return the length of shortest path from src to dst.
+    int n = 0;
+    while (graph[n]) ++n;
+
+    vector<int> dist(n, INT_MAX);
+    dist[src] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+
+        for (int v = 0; v < n; ++v) {
+            if (graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v]) {
+                dist[v] = dist[u] + graph[u][v];
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    return dist[dst];
 }
